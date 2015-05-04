486c6bf13819c1cb2d20811d9e9aa7e3e0e3d9b2
Jeremy Larkin <jeremy@imgix.com>
2015-05-04 11:42:06 -0700

    add assertions for struct sizes

--- tests/test_cdef.lua
+++ tests/test_cdef.lua
@@ -1,5 +1,20 @@
+local ffi = require('ffi')
+
 return {
-	test_cdef = function()
+	test_socket = function()
 		require("levee.cdef")
+		assert(ffi.sizeof("struct in_addr") == 4)
+		assert(ffi.sizeof("struct in6_addr") == 16)
+		assert(ffi.sizeof("struct sockaddr") == 16)
+		assert(ffi.sizeof("struct sockaddr_storage") == 128)
+		assert(ffi.sizeof("struct sockaddr_in") == 16)
+		assert(ffi.sizeof("struct sockaddr_in6") == 28)
+		if ffi.os == "Linux" then
+			assert(ffi.sizeof("struct sockaddr_un") == 110)
+		elseif ffi.os == "OSX" then
+			assert(ffi.sizeof("struct sockaddr_un") == 106)
+		else
+			assert(false) -- unsupported os
+		end
 	end,
 }
