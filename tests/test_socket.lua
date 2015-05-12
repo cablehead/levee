d10e194ce4349037a271aa81900cd2167adddabc
Jeremy Larkin <jeremy@imgix.com>
2015-05-11 19:38:00 -0700

    add basic socket test

---
+++ tests/test_socket.lua
@@ -0,0 +1,13 @@
+return {
+	test_core = function()
+		local Socket = require("levee.socket")
+		local Endpoint = require("levee.endpoint")
+		local l, err = Socket:listen(12345, "127.0.0.1")
+		local c, err = Socket:connect(12345, "127.0.0.1")
+		assert(l ~= nil)
+		assert(c ~= nil)
+		local le, err = Endpoint:sockname(l.base.no)
+		local ce, err = Endpoint:peername(c.base.no)
+		assert(tostring(le) == tostring(ce))
+	end,
+}
