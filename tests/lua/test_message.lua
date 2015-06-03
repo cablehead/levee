82f660c5e49a3d68b938aaf4130660f5a4d5d543
Andy Gayton <andy@thecablelounge.com>
2015-06-03 10:37:29 -0700

    fix up test_pipe

--- tests/lua/test_message.lua
+++ tests/lua/test_message.lua
@@ -3,14 +3,13 @@ return {
 		local h = require("levee").Hub()
 		local p = h:pipe()
 
-		-- send and then recv
+		-- recv and then send
 		h:spawn_later(10, function() p:send("1") end)
 		assert.equal(p:recv(), "1")
 
-		-- recv and then send
+		-- send and then recv
 		local state
 		h:spawn(function() state = p:recv() end)
-		h:sleep(1)
 		p:send("2")
 		h:sleep(1)
 		assert.equal(state, "2")
