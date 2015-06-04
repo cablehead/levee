a9add829672b3af3aca0c67945d8061f733e558b
Andy Gayton <andy@thecablelounge.com>
2015-06-03 18:32:45 -0700

    bring back io module

--- levee/io.lua
+++ levee/io.lua
@@ -1,111 +1,122 @@
---
--- TODO: what a mess. just bashing this out.
---
+local ffi = require("ffi")
+local C = ffi.C
 
-local message = require("levee.message")
-local sys = require("levee.sys")
 local Buffer = require("levee.buffer")
+local sys = require("levee.sys")
 
 
-local Recver = function(hub, pollin, no)
-	local sender, recver = unpack(hub:pipe())
+--
+-- Read
+--
+local R_mt = {}
+R_mt.__index = R_mt
 
-	local buf = Buffer(16384)
 
-	hub:spawn(function()
-		while true do
-			local got = pollin:recv()
-
-			if got == nil then
-				hub:unregister(no)
-				sender:close()
-				break
-			end
-
-			while true do
-				buf:ensure(4096)
-				local n = sys.fd.read(no, buf:tail())
-				if n <= 0 then break end
-				buf:bump(n)
-				sender:send(buf)
-			end
+function R_mt:reader()
+	for ev in self.r_ev do
+		if ev < -1 then
+			self.hub:unregister(self.no, true)
+			self.recver:close()
+			return
 		end
-	end)
 
-	return {
-		recv = function()
-			return recver:recv()
-		end,
+		while true do
+			local n = C.read(self.no, self.buf:tail())
+			if n == 0 then
+				self.hub:unregister(self.no, true)
+				self.recver:close()
+				return
+			end
+			-- read until EAGAIN
+			if n < 0 then break end
+			self.buf:bump(n)
+		end
 
-		close = function()
-			hub:unregister(no)
-			return recver:close()
-		end,
-	}
+		self.recver:send(self.buf)
+	end
 end
 
 
-local Sender = function(hub, pollout, no)
-	local sender, recver = unpack(hub:pipe())
-	local gate = hub:switch()
+function R_mt:__call()
+	return self.recver:recv()
+end
 
-	hub:spawn(function()
-		while true do
-			local s = pollout:recv()
-			if s == nil then
-				gate.sender:close()
-				recver:close()
-				break
-			end
-			gate.sender:send(s)
-		end
-	end)
 
-	hub:spawn(function()
-		while true do
-			local s = recver:recv()
-			if s == nil then
-				hub:unregister(no)
-				break
-			end
-			if sys.fd.write(no, s) < 0 then
-				recver:close()
-				hub:unregister(no)
-				break
-			end
-		end
-	end)
+function R_mt:recv()
+	return self.recver:recv()
+end
 
-	return sender
+
+local function R_init(self)
+	self.buf = Buffer(4096)
+	self.recver = self.hub:pipe()
+	self.hub:spawn(self.reader, self)
+end
+
+
+--
+-- Write
+--
+local W_mt = {}
+W_mt.__index = W_mt
+
+
+function W_mt:send(iovec)
+	C.writev(self.no, iovec.iov, iovec.n)
+end
+
+
+--
+-- Read / Write
+--
+local RW_mt = {}
+RW_mt.__index = RW_mt
+
+RW_mt.reader = R_mt.reader
+RW_mt.__call = R_mt.__call
+RW_mt.recv = R_mt.recv
+RW_mt.send = W_mt.send
+
+
+--
+-- IO module interface
+--
+local IO_mt = {}
+IO_mt.__index = IO_mt
+
+
+function IO_mt:r(no)
+	local m = setmetatable({hub = self.hub, no = no}, R_mt)
+	m.r_ev = self.hub:register(no, true)
+	R_init(m)
+	return m
+end
+
+
+function IO_mt:w(no)
+	local m = setmetatable({hub = self.hub, no = no}, W_mt)
+	local _
+	_, m.w_ev = self.hub:register(no, false, true)
+	return m
+end
+
+
+function IO_mt:rw(no)
+	local m = setmetatable({hub = self.hub, no = no}, RW_mt)
+	m.r_ev, m.w_ev = self.hub:register(no, false, true)
+	R_init(m)
+	return m
+end
+
+
+function IO_mt:pipe()
+	local r, w = sys.os.pipe()
+	sys.os.nonblock(r)
+	sys.os.nonblock(w)
+	return self:r(r), self:w(w)
 end
 
 
 return function(hub)
-	local M = {h=hub}
-
-	function M:r(no)
-		sys.fd.nonblock(no, true)
-		local pollin = self.h:register(no, true)
-		return Recver(hub, pollin, no)
-	end
-
-	function M:w(no)
-		sys.fd.nonblock(no, true)
-		local _, pollout = self.h:register(no, nil, true)
-		return Sender(hub, pollout, no)
-	end
-
-
-	function M:rw(no)
-		sys.fd.nonblock(no, true)
-		local pollin, pollout = self.h:register(no, true, true)
-		local recver = Recver(hub, pollin, no)
-		local sender = Sender(hub, pollout, no)
-		return message.Pair(sender, recver)
-	end
-
-	function M:pipe()
-	end
-
-	return M
+	return setmetatable({hub = hub}, IO_mt)
 end
--- tests/lua/test_io.lua
+++ tests/lua/test_io.lua
@@ -1,47 +1,15 @@
---[[
-local levee = require("levee")
-local sys = require("levee.sys")
-
-local ffi = require("ffi")
-local C = ffi.C
-]]--
-
-
 return {
-	test_io_close_read = function()
-		if true then return 'SKIP' end
-		levee.run(function(h)
-			local r, w = sys.fd.pipe()
-			local r, w = h.io:r(r), h.io:w(w)
+	test_io = function()
+		local levee = require("levee")
 
-			collectgarbage("collect")
+		local h = levee.Hub()
+		local r, w = h.io:pipe()
+		local iov = levee.iovec.Iovec(4)
 
-			-- TODO: this is needed as poller doesn't flush en_in before closing fd
-			w:send("foo")
-			assert.equal(r:recv():take_s(), "foo")
-
-			r:close()
-			assert.equal(r:recv(), nil)
-
-			-- it takes a turn to detect the fd closed
-			w:send("foo")
-			assert.equal(w:send("foo"), nil)
-		end)
-	end,
-
-	test_io_close_write = function()
-		if true then return 'SKIP' end
-		levee.run(function(h)
-			local r, w = sys.fd.pipe()
-			local r, w = h.io:r(r), h.io:w(w)
-
-			collectgarbage("collect")
-
-			-- TODO: this is needed as poller doesn't flush en_in before closing fd
-			w:send("foo")
-			assert.equal(r:recv():take_s(), "foo")
-
-			w:close()
-		end)
+		iov:write("foo")
+		iov:write("bar")
+		w:send(iov)
+		assert.equal(r:recv():take_s(), "foobar")
+		iov:reset()
 	end,
 }
