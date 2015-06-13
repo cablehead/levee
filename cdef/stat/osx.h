1ef35418eea24d95de72e457ea01883ca042c4ba
Andy Gayton <andy@thecablelounge.com>
2015-06-12 18:05:52 -0700

    add stat related constants

--- cdef/stat/osx.h
+++ cdef/stat/osx.h
@@ -8,8 +8,28 @@ typedef long long int off_t;
 typedef long long int blkcnt_t;
 typedef int blksize_t;
 
+enum {
+     S_IFMT   = 0170000,  /* type of file */
+     S_IFIFO  = 0010000,  /* named pipe (fifo) */
+     S_IFCHR  = 0020000,  /* character special */
+     S_IFDIR  = 0040000,  /* directory */
+     S_IFBLK  = 0060000,  /* block special */
+     S_IFREG  = 0100000,  /* regular */
+     S_IFLNK  = 0120000,  /* symbolic link */
+     S_IFSOCK = 0140000,  /* socket */
+     S_IFWHT  = 0160000,  /* whiteout */
+};
 
- struct stat { /* when _DARWIN_FEATURE_64_BIT_INODE is defined */
+enum {
+     S_ISUID = 0004000,  /* set user id on execution */
+     S_ISGID = 0002000,  /* set group id on execution */
+     S_ISVTX = 0001000,  /* save swapped text even after use */
+     S_IRUSR = 0000400,  /* read permission, owner */
+     S_IWUSR = 0000200,  /* write permission, owner */
+     S_IXUSR = 0000100,  /* execute/search permission, owner */
+};
+
+struct stat { /* when _DARWIN_FEATURE_64_BIT_INODE is defined */
 	 dev_t           st_dev;           /* ID of device containing file */
 	 mode_t          st_mode;          /* Mode of file (see below) */
 	 nlink_t         st_nlink;         /* Number of hard links */
--- tests/lua/sys/test_os.lua
+++ tests/lua/sys/test_os.lua
@@ -3,11 +3,11 @@ local C = ffi.C
 
 return {
 	test_stat = function()
-		local no = ffi.C.open("foo121", bit.bor(C.O_RDONLY, C.O_NONBLOCK))
+		local no = C.open("foo121", bit.bor(C.O_RDONLY, C.O_NONBLOCK))
 		assert(no < 0)
 
 		local str = debug.getinfo(1, "S").source:sub(2)
-		local no = ffi.C.open(str, bit.bor(C.O_RDONLY, C.O_NONBLOCK))
+		local no = C.open(str, bit.bor(C.O_RDONLY, C.O_NONBLOCK))
 
 		local st = ffi.new("struct stat")
 		local rc = C.fstat64(no, st)
