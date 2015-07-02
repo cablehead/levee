0769a855e01ee035c7bea8c417d6f10e8d6f8c8f
Andy Gayton <andy@thecablelounge.com>
2015-07-02 15:35:43 -0700

    add h.thread:spawn

--- levee/thread.lua
+++ levee/thread.lua
@@ -8,6 +8,7 @@ Thread_mt.__index = Thread_mt
 function Thread_mt:call(f, ...)
 	local state = levee.State()
 
+	-- bootstrap
 	assert(state:load_function(
 		function(sender, f, ...)
 			local ok, got = pcall(loadstring(f), ...)
@@ -37,6 +38,32 @@ end
 
 
 function Thread_mt:spawn(f)
+	local state = levee.State()
+
+	-- bootstrap
+	assert(state:load_function(
+		function(sender, f)
+			local levee = require("levee")
+			local h = levee.Hub()
+			h.parent = levee.message.Pair(sender, sender:connect(h:channel()))
+
+			local ok, got = pcall(loadstring(f), h)
+
+			if not ok then
+				-- TODO: we should work an optional error message into Pipe close
+				print("ERROR:", got)
+			else
+				-- TODO: close
+			end
+		end))
+
+		local recver = self.hub:channel():bind()
+		state:push(recver:create_sender())
+
+		state:push(string.dump(f))
+		state:run(2, true)
+
+		return levee.message.Pair(recver:recv(), recver)
 end
 
 
--- tests/lua/main/test_thread.lua
+++ tests/lua/main/test_thread.lua
@@ -9,4 +9,17 @@ return {
 		local recver = h.thread:call(add, 3, 2)
 		assert.equal(recver:recv(), 5)
 	end,
+
+	test_spawn = function()
+		local h = require("levee").Hub()
+
+		local function f(h)
+			assert(h.parent:recv() == 123)
+			h.parent:send(321)
+		end
+
+		local child = h.thread:spawn(f)
+		child:send(123)
+		assert.equal(child:recv(), 321)
+	end,
 }
