33add09768c1e3ad6c7cc196f96c4ecdb69415b0
Jeremy Larkin <jeremy@imgix.com>
2015-07-09 11:48:27 -0700

    add cmake scripts

---
+++ cmake/Siphon.cmake
@@ -0,0 +1,26 @@
+include(ExternalProject)
+
+set(SIPHON_DIR "${CMAKE_CURRENT_BINARY_DIR}/siphon")
+set(SIPHON_LIB "${SIPHON_DIR}/lib/libsiphon.a")
+set(SIPHON_INC "${SIPHON_DIR}/include")
+
+externalproject_add(siphon_project
+	GIT_REPOSITORY git@github.com:imgix/siphon.git
+	GIT_TAG 7eea26a
+	PREFIX "${CMAKE_CURRENT_BINARY_DIR}/siphon_project"
+	UPDATE_COMMAND ""
+	CMAKE_ARGS
+		"-G${CMAKE_GENERATOR}"
+		"-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+		"-DCMAKE_INSTALL_PREFIX=${SIPHON_DIR}"
+)
+
+if("${CMAKE_SYSTEM_NAME}" EQUAL "Linux")
+	set(SIPHON_LIB "-Wl,--whole-archive,${SIPHON_LIB},--no-whole-archive")
+elseif("${CMAKE_SYSTEM_NAME}" EQUAL "Darwin")
+	set(SIPHON_LIB "-Wl,-force_load,${SIPHON_LIB}")
+endif()
+
+add_library(libsiphon STATIC IMPORTED)
+set_target_properties(libsiphon PROPERTIES IMPORTED_LOCATION ${SIPHON_LIB})
+add_dependencies(libsiphon siphon_project)
