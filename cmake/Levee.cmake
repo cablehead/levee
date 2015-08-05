679b659971895c4825db54b78e2e0b027969e999
Andy Gayton <andy@thecablelounge.com>
2015-08-04 17:51:33 -0700

    tweak cmake so levee will rebuild on libleveebase changes

--- cmake/Levee.cmake
+++ cmake/Levee.cmake
@@ -1,7 +1,7 @@
 set(LEVEE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
 set(LEVEE_LIB "${LEVEE_DIR}/liblevee.a")
-set(LEVEE_INC "${PROJECT_SOURCE_DIR}/src")
 set(LEVEEBASE_LIB "${LEVEE_DIR}/libleveebase.a")
+set(LEVEE_INC "${PROJECT_SOURCE_DIR}/src")
 
 set(LEVEE_CDEF_MANIFEST ${PROJECT_SOURCE_DIR}/cdef/manifest.lua)
 set(LEVEE_CDEF_HEADER ${CMAKE_CURRENT_BINARY_DIR}/levee_cdef.h)
@@ -10,8 +10,6 @@ file(GLOB_RECURSE LEVEE_CDEF ${PROJECT_SOURCE_DIR}/cdef/*.h)
 set(LEVEE_BUNDLE_SCRIPT ${PROJECT_SOURCE_DIR}/bin/bundle.lua)
 set(LEVEE_BUNDLE_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/bundle.c)
 file(GLOB_RECURSE LEVEE_BUNDLE ${PROJECT_SOURCE_DIR}/levee/*.lua)
-
-set(LEVEE_LIB_SCRIPT ${PROJECT_SOURCE_DIR}/bin/lib.sh)
 
 add_custom_command(
 	OUTPUT ${LEVEE_CDEF_HEADER}
@@ -37,15 +35,5 @@ add_library(
 	src/list.c
 	${LEVEE_BUNDLE_OUTPUT}
 )
-add_dependencies(libleveebase libluajit libsiphon)
 set_target_properties(libleveebase PROPERTIES OUTPUT_NAME leveebase)
-
-add_custom_target(
-	liblevee ALL
-	COMMAND
-		libtool -static -o ${LEVEE_LIB}
-		$<TARGET_FILE:libleveebase> $<TARGET_FILE:libluajit> $<TARGET_FILE:libsiphon>
-	DEPENDS libleveebase libluajit libsiphon
-	BYPRODUCTS ${LEVEE_LIB}
-)
-
+add_dependencies(libleveebase libluajit libsiphon)
