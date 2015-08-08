cc5cc498d273affcccb332f2a89cdb196d253389
Jeremy Larkin <jeremy@imgix.com>
2015-08-07 17:02:46 -0700

    allow run command to execute scripts directly

--- levee/cmd/run.lua
+++ levee/cmd/run.lua
@@ -1,13 +1,24 @@
 return function(argv)
 	local os = require('os')
 
-	local path = argv:string()
-	if path:sub(-1) == '/' then
-		path = path:sub(1, -2)
+	local path = argv:next()
+	local dir = path:match("^(.*)/[^/]+$")
+	local main
+
+	if path:match("%.lua$") or path:match("lua%.test$") then
+		main = path
+	else
+		if not dir then
+			io.stderr:write("invalid module path\n")
+			os.exit(1)
+		end
+
+		if path:sub(-1) == '/' then
+			path = path:sub(1, -2)
+		end
+		main = path .. "/main.lua"
 	end
 
-	local dir, name = path:match("(.*)/(.*)")
-	local main = path .. "/main.lua"
 	package.path = dir .. "/?.lua;" .. dir .. "/?/init.lua;" .. package.path
 
 	arg = argv:remain()
