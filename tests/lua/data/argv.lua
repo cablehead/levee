4a397ca1628676c46ddd8c9ab5860984ec065e4a
Jeremy Larkin <jeremy@imgix.com>
2015-08-07 17:03:07 -0700

    add argv test

---
+++ tests/lua/data/argv.lua
@@ -0,0 +1,17 @@
+local Argv = require('levee.argv')
+
+return {
+	test_basic = function()
+		local argv = Argv({
+			"value", "1.23", "a", "b", "--stuff", "after", "-xyz", "final"})
+		assert.same("value", argv:next())
+		assert.same(1.23, argv:number())
+		assert.same({"a", "b"}, argv:list())
+		assert.same("stuff", argv:option())
+		assert.same("after", argv:next())
+		assert.same("x", argv:option())
+		assert.same("y", argv:option())
+		assert.same("z", argv:option())
+		assert.same("final", argv:next())
+	end,
+}
