2708cfcf47f728d2524817116459beb2a8fc9bf6
Jeremy Larkin <jeremy@imgix.com>
2015-08-20 18:09:26 -0700

    emit errors in argv

--- levee/argv.lua
+++ levee/argv.lua
@@ -1,92 +1,119 @@
 local io = require('io')
+local os = require('os')
 
 local Argv_mt = {}
 Argv_mt.__index = Argv_mt
 
 
+function Argv_mt:exit(fmt, ...)
+	local msg = string.format(fmt, unpack{...})
+	if self.exit_cb then
+		self.exit_cb(self.idx, self.opt, msg)
+	else
+		if self.opt then
+			io.stderr:write(string.format("invalid option '%s': %s\n", self.opt, msg))
+		else
+			io.stderr:write(string.format("invalid argument #%d: %s\n", self.idx, msg))
+		end
+		os.exit(1)
+	end
+end
+
+
 function Argv_mt:more()
-	return self.curr <= self.stop
+	return self.idx <= self.idxstop
 end
 
 
 function Argv_mt:peek()
 	if self:more() then
-		return self.args[self.curr]
+		return self.args[self.idx]
 	end
 end
 
 
-function Argv_mt:next(fn)
-	if self.opt then return end
-	local value
-	if self:more() then
-		value = self.args[self.curr]
-		if fn then
-			value = fn(value)
-			if not value then return end
-		end
-		self.curr = self.curr + 1
+function Argv_mt:next(fn, msg)
+	if self.optrem or not self:more() then
+		return self:exit(msg)
 	end
+	local value = self.args[self.idx]
+	if fn then
+		value = fn(value)
+		if not value then
+			return self:exit(msg)
+		end
+	end
+	self.idx = self.idx + 1
 	return value
 end
 
 
 function Argv_mt:option()
-	local opt = self.opt
+	local optrem = self.optrem
+	self.opt = nil
 
-	if not opt then
+	if not optrem then
 		if not self:more() then return end
 
-		local arg = self.args[self.curr]
+		local arg = self.args[self.idx]
 
 		-- match long option: --long-value
-		opt = arg:match("^%-%-(.*)")
-		if opt then
-			self.curr = self.curr + 1
-			return opt
+		optrem = arg:match("^%-%-(.*)")
+		if optrem then
+			self.idx = self.idx + 1
+			self.opt = optrem -- save active option
+			return optrem
 		end
 
 		-- match short options: -xyz
-		opt = arg:match("^%-(.+)")
-		if opt then
-			self.curr = self.curr + 1
+		optrem = arg:match("^%-(.+)")
+		if optrem then
+			self.idx = self.idx + 1
 		end
 
 		-- no option format matched
-		if not opt then return end
+		if not optrem then return end
 	end
 
 	-- split off the next character
-	local ch = opt:sub(1, 1)
-	opt = opt:sub(2)
+	local ch = optrem:sub(1, 1)
+	optrem = optrem:sub(2)
 
 	-- save the remaining 
-	if #opt > 0 then
-		self.opt = opt
+	if #optrem > 0 then
+		self.optrem = optrem
 	else
-		self.opt = nil
+		self.optrem = nil
 	end
+	self.opt = ch -- save active option
 	return ch
 end
 
 
-function Argv_mt:list()
-	if self.opt then return end
+function Argv_mt:list(min, max)
+	if self.optrem then return end
 	local list = {}
-	while self:more() do
-		value = self.args[self.curr]
+	while self.idx + #list <= self.idxstop do
+		local value = self.args[self.idx + #list]
 		if value:sub(1,1) == '-' then
 			break
 		end
 		table.insert(list, value)
-		self.curr = self.curr + 1
+		if max and #list == max then
+			break
+		end
 	end
+	if min and #list < min then
+		self:exit("list expected at least %d item(s)", min)
+		return
+	end
+	self.idx = self.idx + #list
 	return list
 end
 
 
 function Argv_mt:remain()
-	local remain = {unpack(self.args, self.curr)}
+	local remain = {unpack(self.args, self.idx)}
 	remain[0] = self.args[0]
 	return remain
 end
@@ -95,14 +122,14 @@ end
 function Argv_mt:number()
 	return self:next(function(val)
 		return tonumber(val)
-	end)
+	end, "number expected")
 end
 
 
 function Argv_mt:file(mode)
 	return self:next(function(val)
 		return io.open(val, mode)
-	end)
+	end, "file expected")
 end
 
 
@@ -113,7 +140,7 @@ function Argv_mt:input(defult)
 		else
 			return io.open(val, "r")
 		end
-	end)
+	end, "readablefile expected")
 end
 
 
@@ -124,13 +151,28 @@ function Argv_mt:output(defult)
 		else
 			return io.open(val, "w")
 		end
-	end)
+	end, "writable file expected")
 end
 
 
-return function(args, start, stop)
+return function(args, start, stop, exit)
+	if type(exit) ~= "function" then
+		if type(stop) == "function" then
+			exit = stop
+			stop = nil
+		elseif type(start) == "function" then
+			exit = start
+			start = nil
+			stop = nil
+		end
+	end
 	start = start or 1
 	stop = stop or #args
-	return setmetatable({args=args, start=start, stop=stop, curr=start}, Argv_mt)
+	exit = exit or arg_exit
+	return setmetatable({
+		args = args,
+		idx = start, idxstart = start, idxstop = stop,
+		exit_cb = exit
+	}, Argv_mt)
 end
 
--- tests/lua/data/argv.lua
+++ tests/lua/data/argv.lua
@@ -2,8 +2,12 @@ local Argv = require('levee.argv')
 
 return {
 	test_basic = function()
+		local function exit(idx, opt, msg)
+			return -- ignore errors for this test
+		end
 		local argv = Argv({
-			"value", "1.23", "a", "b", "--stuff", "after", "-xyz", "final"})
+			"value", "1.23", "a", "b", "--stuff", "after", "-xyz", "final"},
+			exit)
 		assert.same("value", argv:next())
 		assert.same(1.23, argv:number())
 		assert.same({"a", "b"}, argv:list())
@@ -16,5 +20,71 @@ return {
 		assert.same("z", argv:option())
 		assert.is_nil(argv:option())
 		assert.same("final", argv:next())
+	end,
+
+	test_list = function()
+		local argv, last_opt, last_idx
+		local function exit(idx, opt, msg)
+			last_idx = idx
+			last_opt = opt
+		end
+
+		-- test any length list
+		argv = Argv({"--list", "a", "b", "c", "--other"}, exit)
+		assert.same("list", argv:option())
+		assert.same({"a","b","c"}, argv:list())
+		assert.same("other", argv:option())
+		assert.is_nil(last_opt)
+		assert.is_nil(last_idx)
+
+		-- test length limited list
+		argv = Argv({"--list", "a", "b", "c", "--other"}, exit)
+		assert.same("list", argv:option())
+		assert.same({"a","b"}, argv:list(1, 2))
+		assert.same("c", argv:next())
+		assert.same("other", argv:option())
+		assert.is_nil(last_opt)
+		assert.is_nil(last_idx)
+
+		-- test a list that is too short
+		argv = Argv({"--list", "a", "b"}, exit)
+		assert.same("list", argv:option())
+		assert.is_nil(argv:list(3))
+		assert.same("list", last_opt)
+		assert.same(2, last_idx)
+		last_opt = nil
+		last_idx = nil
+
+		-- test zero length list
+		argv = Argv({"--list", "--other"}, exit)
+		assert.same("list", argv:option())
+		assert.same({}, argv:list())
+		assert.same("other", argv:option())
+	end,
+
+	test_bad_value = function()
+		local argv, last_opt, last_idx
+		local function exit(idx, opt, msg)
+			last_idx = idx
+			last_opt = opt
+		end
+
+		-- test non-number
+		argv = Argv({"--num", "xx"}, exit)
+		assert.same("num", argv:option())
+		assert.is_nil(argv:number())
+		assert.same("num", last_opt)
+		assert.same(2, last_idx)
+		last_opt = nil
+		last_idx = nil
+
+		-- test no value
+		argv = Argv({"--num"}, exit)
+		assert.same("num", argv:option())
+		assert.is_nil(argv:number())
+		assert.same("num", last_opt)
+		assert.same(2, last_idx)
+		last_opt = nil
+		last_idx = nil
 	end,
 }
