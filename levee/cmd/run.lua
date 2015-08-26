5ffca957bab35285dba238ef79d417e969cb97a0
Andy Gayton <andy@thecablelounge.com>
2015-08-25 23:57:49 -0700

    add a -e option to levee run

--- levee/cmd/run.lua
+++ levee/cmd/run.lua
@@ -5,43 +5,50 @@ local levee = require("levee")
 
 return {
 	usage = function()
-		return "Usage: levee run <path> [arg...]"
+		return "Usage: levee run <path> | -e <script> [arg...]"
 	end,
 
 	parse = function(argv)
 		local options = {}
 
 		if not argv:more() then return end
-		local path = argv:next():gsub("/$", "")
 
-		local st = levee.sys.os.stat(path)
-		if not st or not (st:is_reg() or st:is_dir()) then
-			io.stderr:write(("invalid path: %s\n"):format(path))
+		local opt = argv:option()
+
+		if opt then
+			if opt ~= "e" then return end
+			options.f, options.err = loadstring(argv:next())
+
+		else
+			local path = argv:next():gsub("/$", "")
+
+			local st = levee.sys.os.stat(path)
+			if not st or not (st:is_reg() or st:is_dir()) then
+				io.stderr:write(("invalid path: %s\n"):format(path))
+				os.exit(1)
+			end
+
+			if st:is_dir() then
+				package.path = (levee.sys.os.dirname(path) .. "/?.lua;" ..
+					levee.sys.os.dirname(path) .. "/?/init.lua;" .. package.path)
+				path = path .. "/main.lua"
+			end
+
+			options.f, options.err = loadfile(path)
+		end
+
+		if not options.f then
+			io.stderr:write(options.err)
+			io.stderr:write("\n")
 			os.exit(1)
 		end
 
-		if st:is_dir() then
-			package.path = (levee.sys.os.dirname(path) .. "/?.lua;" ..
-				levee.sys.os.dirname(path) .. "/?/init.lua;" .. package.path)
-			options.main = path .. "/main.lua"
-		else
-			options.main = path
-		end
-
 		options.arg = argv:remain()
-
 		return options
 	end,
 
 	run = function(options)
 		arg = options.arg
-		local f, err = loadfile(options.main)
-		if f then
-			f()
-		else
-			io.stderr:write(err)
-			io.stderr:write("\n")
-		end
+		return options.f()
 	end,
 }
-
