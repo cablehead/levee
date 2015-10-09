aa8da155aef59adbd41603f7b18fe25ce530db2c
Andy Gayton <andy@thecablelounge.com>
2015-10-09 14:54:51 -0700

    bump version of Siphon to pull in msgpack

--- cmake/Siphon.cmake
+++ cmake/Siphon.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 set(SIPHON_DIR "${CMAKE_CURRENT_BINARY_DIR}/siphon")
 set(SIPHON_LIB "${SIPHON_DIR}/lib/libsiphon.a")
 set(SIPHON_INC "${SIPHON_DIR}/include")
-set(SIPHON_HASH e4b34b7)
+set(SIPHON_HASH efed897)
 
 externalproject_add(siphon_project
 	GIT_REPOSITORY git@github.com:imgix/siphon.git
