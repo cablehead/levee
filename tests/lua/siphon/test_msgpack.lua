53cee61403edf0c35531dd3e50da5c6799dd5ff6
Andy Gayton <andy@thecablelounge.com>
2015-10-14 17:04:29 -0700

    test just encoding a string with msgpack

--- tests/lua/siphon/test_msgpack.lua
+++ tests/lua/siphon/test_msgpack.lua
@@ -1,7 +1,16 @@
 local levee = require("levee")
 
 return {
-	test_encode = function()
+	test_string = function()
+		local want = "foo"
+		local buf = levee.msgpack.encode(want)
+		local ok, got = levee.msgpack.decoder():stream_consume(buf)
+		assert(ok)
+		assert.same(want, got)
+		assert.equal(#buf, 0)
+	end,
+
+	test_nested = function()
 		local want = {
 			foo = "bar",
 			arr = {3, -4, "foo", true, false, 3.7},
