d8904de96bb66637e6432de4b581ca26ee592802
Jeremy Larkin <jeremy@imgix.com>
2015-10-19 17:33:07 -0700

    add version sub command

--- levee/cmd/init.lua
+++ levee/cmd/init.lua
@@ -3,4 +3,5 @@ return {
 	test = require("levee.cmd.test"),
 	bundle = require("levee.cmd.bundle"),
 	build = require("levee.cmd.build"),
+	version = require("levee.cmd.version"),
 }
---
+++ levee/cmd/version.lua
@@ -0,0 +1,15 @@
+return {
+	usage = function()
+		return "Usage: levee version"
+	end,
+
+	parse = function(argv)
+		return {}
+	end,
+
+	run = function(options)
+		print("Levee version 0.3 2015-10-19")
+		print("Copyright (c) 2015 Imgix")
+	end
+}
+
