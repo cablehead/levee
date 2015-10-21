7806b8dded7d2c27f775d23d70f1fa89b854d0ad
Andy Gayton <andy@thecablelounge.com>
2015-10-20 19:01:46 -0700

    massive file layout move; everything is broken!

--- levee/meta.lua
+++ levee/meta.lua
@@ -1,3 +1,17 @@
+local date = {
+	year = 2015,
+	month = 10,
+	day = 19
+}
+
+date.string = string.format(
+	"%d-%d-%d",
+	date.year, date.month, date.day
+)
+
 return {
-	version = "0.0.1"
+	major = 0,
+	minor = 2,
+	patch = 0,
+	date = date
 }
