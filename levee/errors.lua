4bd3853fc7836b7d6d2b68daf5de2b7feffdbc76
Andy Gayton <andy@thecablelounge.com>
2015-10-26 12:57:48 -0400

    add an is convenience to errors

--- levee/errors.lua
+++ levee/errors.lua
@@ -3,28 +3,46 @@ local C = ffi.C
 
 
 local Error_mt = {}
-Error_mt.__index = Error_mt
-
-
-function Error_mt:__index(key)
-	return self["_"..key]
-end
 
 
 function Error_mt:__tostring()
 	return string.format(
-		"%s error [%s]: %s (%s)\n",
-		ffi.string(self.domain),
-		self.code,
-		ffi.string(self.name),
-		ffi.string(self.msg))
+		"%s error [%s]: %s (%s)\n", self.domain, self.code, self.name, self.msg)
+end
+
+
+function Error_mt:is(domain, name)
+	return self.domain == domain and self.name == name
+end
+
+
+function Error_mt:next()
+	return C.sp_error_next(self)
+end
+
+
+function Error_mt:__index(key)
+	local fields = {
+		code = tonumber,
+		domain = ffi.string,
+		name = ffi.string,
+		msg = ffi.string, }
+
+	if fields[key] then
+		return fields[key](self["_"..key])
+	end
+
+	local domain, name = key:match("is_([a-zA-Z0-9]+)_([a-zA-Z0-9]+)")
+	if domain and name then return self:is(domain, name) end
+
+	return Error_mt[key]
 end
 
 
 ffi.metatype("SpError", Error_mt)
 
 
-return {
+local M = {
 	get = function(code)
 		return C.sp_error(code)
 	end,
@@ -37,3 +55,21 @@ return {
 		return C.sp_error_add(code, domain, name, msg)
 	end,
 }
+
+
+local M_mt = {
+	__call = function()
+		local e
+		return function()
+			if not e then
+				e = M.get(-1)
+			else
+				e = e:next()
+			end
+			return e
+		end
+	end,
+}
+
+
+return setmetatable(M, M_mt)
--- tests/test_errors.lua
+++ tests/test_errors.lua
@@ -1,9 +1,31 @@
+local errors = require("levee.errors")
+
+
 return {
 	test_core = function()
-		local errors = require("levee.errors")
-		local want = errors.add(2001, "test", "BAD", "some bad thing")
-		assert.equal(want.code, -2001)
+		local e = errors.add(2001, "test", "BAD", "some bad thing")
+		assert.equal(e.code, -2001)
 		local got = errors.get(2001)
-		assert.equal(want, got)
+		assert.equal(e, got)
+
+		assert(e:is("test", "BAD"))
+		assert(not e:is("test", "FOO"))
+
+		assert(e.is_test_BAD)
+		assert(not e.is_test_FOO)
 	end,
+
+	test_next = function()
+		local e = errors.get(-1)
+		assert.equal(e:next().code, -2)
+	end,
+
+	--[[
+	test_iter = function()
+		errors.add(2001, "test", "BAD", "some bad thing")
+		for err in errors() do
+			print(err)
+		end
+	end,
+	]]--
 }
