091ec3710f49fe9bee1a296dfd115df2635ef419
Andy Gayton <andy@thecablelounge.com>
2015-11-09 21:11:37 -0800

    comment out old high level URI functionality and merge it with the siphon level modules

--- tests/p/test_uri.lua
+++ tests/p/test_uri.lua
@@ -114,5 +114,77 @@ return {
 		assert(other:parse(other_val))
 		join_parser, join_value = parser:join_parser(value, other, other_val)
 		assert.equal("http://user:pass@test.com:80/some/path?a=1", join_value)
-	end
+	end,
+
+--[[
+	-- TODO: higher level URI tests
+	test_segments = function()
+		local u = URI("http://user:pass@test.com:80/some/path?a=b#c")
+		assert(u)
+		assert.equal("http", u.scheme)
+		assert.equal("user", u.user)
+		assert.equal("pass", u.password)
+		assert.equal("test.com", u.host)
+		assert.equal("80", u.port)
+		assert.equal("/some/path", u.path)
+		assert.equal("a=b", u.query)
+		assert.equal("c", u.fragment)
+		assert.equal("user:pass", u.userinfo)
+		assert.equal("user:pass@test.com:80", u.authority)
+	end,
+
+	test_join_string = function()
+		local u = URI("http://user:pass@test.com:80/some/path?a=b#c")
+		local join
+
+		assert(u)
+
+		join = u:join("http://new.com/new/path")
+		assert.equal("http://new.com/new/path", join.value)
+
+		join = u:join("//new.com/new/path")
+		assert.equal("http://user:pass@new.com/new/path", join.value)
+
+		join = u:join("/new/path")
+		assert.equal("http://user:pass@test.com:80/new/path", join.value)
+
+		join = u:join("/../sibling")
+		assert.equal("http://user:pass@test.com:80/sibling", join.value)
+
+		join = u:join("?a=1")
+		assert.equal("http://user:pass@test.com:80/some/path?a=1", join.value)
+	end,
+
+	test_join_parser = function()
+		local u = URI("http://user:pass@test.com:80/some/path?a=b#c")
+		local other, join
+
+		assert(u)
+
+		other = URI("http://new.com/new/path")
+		assert(other)
+		join = u:join(other)
+		assert.equal("http://new.com/new/path", join.value)
+
+		other = URI("//new.com/new/path")
+		assert(other)
+		join = u:join(other)
+		assert.equal("http://user:pass@new.com/new/path", join.value)
+
+		other = URI("/new/path")
+		assert(other)
+		join = u:join(other)
+		assert.equal("http://user:pass@test.com:80/new/path", join.value)
+
+		other = URI("/../sibling")
+		assert(other)
+		join = u:join(other)
+		assert.equal("http://user:pass@test.com:80/sibling", join.value)
+
+		other = URI("?a=1")
+		assert(other)
+		join = u:join(other)
+		assert.equal("http://user:pass@test.com:80/some/path?a=1", join.value)
+	end,
+--]]
 }
