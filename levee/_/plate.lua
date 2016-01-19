d78459ee2f87e5c5c8b1d787d512da8152a9d1fa
Andy Gayton <andy@thecablelounge.com>
2016-01-18 19:25:56 -0800

    add a basic if statement to plate

--- levee/_/plate.lua
+++ levee/_/plate.lua
@@ -2,22 +2,80 @@ local lpeg = require("lpeg")
 
 local P = lpeg.P
 local S = lpeg.S
+local V = lpeg.V
 local C = lpeg.C
 local Ct = lpeg.Ct
 
 
-return function(s, d)
-	local function mapper(s)
-		return d[s]
+local WHITE = S(" \t\r\n") ^ 0
+
+local VAR_BEGIN = P("{{") * WHITE
+local VAR_END = WHITE * P("}}")
+local VAR_MIDDLE = P((1-VAR_END)^0) / function(s) return {'SUB', {s}} end
+local VAR = VAR_BEGIN * VAR_MIDDLE * VAR_END
+
+local STMT_BEGIN = P("{%") * WHITE
+local STMT_END = WHITE * P("%}")
+local END = STMT_BEGIN * P("end") * STMT_END
+
+local IF_BEGIN = STMT_BEGIN * P("if ") * C(P((1-STMT_END)^0)) * STMT_END
+
+
+local parse = Ct(P({
+	"DOC",
+
+	IF = (IF_BEGIN * V("BLOCK") * END) / function(var, ...)
+		return {"IF", {var, {...}}}
+	end,
+
+	TAG = VAR + V("IF"),
+
+	NOT_TAG = P((1 - V("TAG"))^1),
+	BLOCK = P((C(V("NOT_BLOCK_END")) + V("TAG"))^1),
+	NOT_BLOCK_END = P((1 - (V("TAG")+END))^1),
+	DOC = P((C(V("NOT_TAG")) + V("TAG"))^1),
+}))
+
+
+local nodes = {}
+
+
+function nodes.BLOCK(d, block)
+	local ret = {}
+	for __, v in ipairs(block) do
+		if type(v) == "table" then
+			local tree = nodes[v[1]](d, unpack(v[2]))
+			if tree then
+				if type(tree) == "table" then
+					for __, sub_v in ipairs(tree) do
+						table.insert(ret, sub_v)
+					end
+				else
+					table.insert(ret, tree)
+				end
+			end
+		else
+			table.insert(ret, v)
+		end
 	end
+	return ret
+end
 
-	local WHITE = S(" \t\r\n") ^ 0
-	local BEGIN_VAR = P("{{") * WHITE
-	local END_VAR = WHITE * P("}}")
-	local MIDDLE_VAR = P((1-END_VAR)^0) / mapper
-	local VAR = BEGIN_VAR * MIDDLE_VAR * END_VAR
-	local BODY = C(((1 - BEGIN_VAR)) ^ 0)
-	local parse = Ct((BODY * VAR * BODY)^0)
 
-	return table.concat(parse:match(s))
+function nodes.SUB(d, var)
+	return d[var]
+end
+
+
+function nodes.IF(d, var, block)
+	if d[var] then
+		return nodes.BLOCK(d, block)
+	end
+end
+
+
+return function(s, d)
+	d = d or {}
+	local doc = parse:match(s)
+	return table.concat(nodes.BLOCK(d, doc))
 end
--- tests/_/test_plate.lua
+++ tests/_/test_plate.lua
@@ -2,8 +2,16 @@ local _ = require("levee")._
 
 
 return {
-	test_core = function()
+	test_sub = function()
 		local s = "1 {{a}} 2 {{ b }}3"
 		assert.equal(_.plate(s, {a = "A", b = "B"}), "1 A 2 B3")
+	end,
+
+	test_if = function()
+		local s = "1 {% if foo %} 2 {{ b }}hrm {% end %} 3"
+		assert.equal(
+			_.plate(s, {foo = true, b = "noog"}), "1  2 nooghrm  3")
+		assert.equal(
+			_.plate(s, {b = "noog"}), "1  3")
 	end,
 }
