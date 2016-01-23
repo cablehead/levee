9e59330984aaa81b35284c42045a23d3a64b8454
Andy Gayton <andy@thecablelounge.com>
2016-01-23 13:49:44 -0800

    make use of no in winsize, if available

--- levee/_/term.lua
+++ levee/_/term.lua
@@ -11,7 +11,7 @@ local term = {}
 
 
 function term.winsize(no)
-	local rc = C.ioctl(1, C.TIOCGWINSZ, ws)
+	local rc = C.ioctl(no or 1, C.TIOCGWINSZ, ws)
 	if rc < 0 then return errors.get(ffi.errno()) end
 	return nil, {row=ws[0].ws_row, col=ws[0].ws_col}
 end
