a1d0daf82dece52258290a567b83fe208b527f9e
Andy Gayton <andy@thecablelounge.com>
2016-02-02 17:10:18 -0800

    make the caller responsible for closing stdout

--- levee/core/process.lua
+++ levee/core/process.lua
@@ -68,7 +68,6 @@ function M_mt:poweron()
 					child.exit_sig = sig
 					child.done:send(child)
 					if child.stdin then child.stdin:close() end
-					if child.stdout then child.stdout:close() end
 				end
 			end
 		end
