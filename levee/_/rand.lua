d9f50fe37babddfc50591ae8cee6dbfb65fd5c84
Jeremy Larkin <jeremy@imgix.com>
2016-03-07 10:12:22 -0800

    add random string function

--- levee/_/rand.lua
+++ levee/_/rand.lua
@@ -4,11 +4,23 @@ local C = ffi.C
 
 local tmp_int = ffi.new("uint32_t [1]")
 local tmp_num = ffi.new("double [1]")
+local tmp_len = 0
+local tmp_str = nil
 
 local function bytes(len)
 	local val = ffi.new("uint8_t [?]", len)
 	if C.sp_rand(val, len) == 0 then
 		return val
+	end
+end
+
+local function string(len)
+	if len > tmp_len then
+		tmp_len = math.pow(2, math.ceil(math.log(len)/math.log(2)))
+		tmp_str = ffi.cast("uint8_t *", ffi.gc(C.malloc(tmp_len), C.free))
+	end
+	if C.sp_rand(tmp_str, len) == 0 then
+		return ffi.string(tmp_str, len)
 	end
 end
 
@@ -26,6 +38,7 @@ end
 
 return {
 	bytes = bytes,
+	string = string,
 	integer = integer,
 	number = number
 }
