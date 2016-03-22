249f16be3f506831cca559875f71386189c4902c
Andy Gayton <andy@thecablelounge.com>
2016-03-21 17:12:19 -0700

    support utf8_init_fixed; wrap errors returned from encode and decode

--- levee/p/utf8.lua
+++ levee/p/utf8.lua
@@ -1,6 +1,8 @@
 local ffi = require("ffi")
 local C = ffi.C
 
+local errors = require("levee.errors")
+
 
 local Utf8_mt = {}
 Utf8_mt.__index = Utf8_mt
@@ -16,13 +18,22 @@ function Utf8_mt:_init()
 end
 
 
+function Utf8_mt:_init_fixed(buf, len)
+	C.sp_utf8_init_fixed(self, buf, len)
+end
+
+
 function Utf8_mt:encode(buf, len)
-	return C.sp_utf8_encode(self, buf, len or #buf, C.SP_UTF8_NONE)
+	local rc = C.sp_utf8_encode(self, buf, len or #buf, C.SP_UTF8_NONE)
+	if rc >= 0 then return nil, rc end
+	return errors.get(rc)
 end
 
 
 function Utf8_mt:decode(buf, len)
-	return C.sp_utf8_decode(self, buf, len, C.SP_UTF8_NONE)
+	local rc = C.sp_utf8_decode(self, buf, len, C.SP_UTF8_NONE)
+	if rc >= 0 then return nil, rc end
+	return errors.get(rc)
 end
 
 
@@ -31,15 +42,29 @@ function Utf8_mt:peek()
 end
 
 
-local Utf8 = ffi.metatype("SpUtf8", Utf8_mt)
+local Utf8 = {}
+Utf8.__index = Utf8
+
+
+Utf8.new = ffi.metatype("SpUtf8", Utf8_mt)
+
+
+function Utf8.__call()
+	local self = Utf8.new()
+	self:_init()
+	return self
+end
+
+
+function Utf8.from_fixed(buf, len)
+	local self = Utf8.new()
+	self:_init_fixed(buf, len)
+	return self
+end
 
 
 return {
-	Utf8 = function()
-		local self = Utf8()
-		self:_init()
-		return self
-	end,
+	Utf8 = setmetatable({}, Utf8),
 
 	charlen = function(buf, len)
 		return C.sp_utf8_charlen(buf, len or #buf)
--- tests/p/test_utf8.lua
+++ tests/p/test_utf8.lua
@@ -6,13 +6,29 @@ local p = levee.p
 return {
 	test_core = function()
 		local u1 = p.utf8.Utf8()
-		local want = "hi: 😬👻"
-		assert.equal(u1:encode(want), 30)   -- note 30 is the number bytes written
-		local ffi = require("ffi")
-
+		local s = "hi: 😬👻"
+		assert.same({u1:encode(s)}, {nil, 30LL})
 		local u2 = p.utf8.Utf8()
-		assert.equal(u2:decode(u1.buf, u1.len), 12)
-		assert.equal(u2:peek(), want)
+		assert.same({u2:decode(u1.buf, u1.len)}, {nil, 12LL})
+		assert.equal(u2:peek(), s)
+	end,
+
+	test_fixed = function()
+		local b1 = d.Buffer(200)
+		local s = "hi: 😬👻"
+		local u1 = p.utf8.Utf8.from_fixed(b1:tail(), 20)
+		local err = u1:encode(s)
+		assert(err)
+		local u1 = p.utf8.Utf8.from_fixed(b1:tail(), 40)
+		assert.same({u1:encode(s)}, {nil, 30LL})
+
+		local b2 = d.Buffer(200)
+		local u2 = p.utf8.Utf8.from_fixed(b2:tail(), 10)
+		local err = u2:decode(u1.buf, u1.len)
+		assert(err)
+		local u2 = p.utf8.Utf8.from_fixed(b2:tail(), 20)
+		assert.same({u2:decode(u1.buf, u1.len)}, {nil, 12LL})
+		assert.equal(u2:peek(), s)
 	end,
 
 	test_charlen = function()
