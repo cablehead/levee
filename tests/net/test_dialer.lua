44a90f3f24e05082895140ad22b31241f2b9ecc0
Andy Gayton <andy@thecablelounge.com>
2016-04-07 14:29:11 -0700

    begin to flesh out writer in lua

--- tests/net/test_dialer.lua
+++ tests/net/test_dialer.lua
@@ -1,9 +1,36 @@
 return {
 	test_core = function()
+		local ffi = require("ffi")
+		local levee = require("levee")
+		local _ = levee._
 		print()
 		print()
 		local state = C.levee_dialer_init()
 		print(state.rc)
+		print(state.io[0])
+		print(state.io[1])
 		print()
+
+		local req = ffi.new("struct LeveeDialerRequest")
+		local res = ffi.new("struct LeveeDialerResponse")
+
+		local r, w = _.pipe()
+
+		local function write(node, service)
+			req.node_len = #node
+			req.service_len = #service
+			req.socktype = C.SOCK_STREAM
+			req.family = C.AF_INET
+			req.no = w
+
+			print(_.write(state.io[1], req, ffi.sizeof(req)))
+			print(_.write(state.io[1], node))
+			print(_.write(state.io[1], service))
+			print(_.read(r, res, ffi.sizeof(res)))
+			print(res.no)
+		end
+
+		write("localhost", "8000")
+
 	end,
 }
