f96c4e7e20c9713eb3054b43f747630524d15a03
Jeremy Larkin <jeremy@imgix.com>
2016-04-25 14:28:40 -0700

    try improved core dump tracking

--- .travis/run.osx.sh
+++ .travis/run.osx.sh
@@ -1,9 +1,12 @@
 levee test -v tests/
 if [[ $? -eq 139 ]]; then
-	if [[ -d $HOME/Library/Logs/DiagnosticReports/ ]]; then
-		cat $HOME/Library/Logs/DiagnosticReports/levee*
-	fi
-	if [[ -d /Library/Logs/DiagnosticReports/ ]]; then
-		cat /Library/Logs/DiagnosticReports/levee*
-	fi
+	while : ; do
+		sleep 1
+		REPORTS=$(ls $HOME/Library/Logs/DiagnosticReports/main*)
+		if [[ ! -z "$REPORTS" ]]; then
+			cat $REPORTS
+			rm $REPORTS
+			exit
+		fi
+	done
 fi
