b218f0fee2f8a1441a81f753ff3982b62494cbe6
Andy Gayton <andy@thecablelounge.com>
2017-01-06 17:54:49 -0800

    bind to port 0 for the test

--- tests/net/test_tls.lua
+++ tests/net/test_tls.lua
@@ -5,6 +5,11 @@ local d = levee.d
 local filename = debug.getinfo(1, 'S').source:sub(2)
 local path = _.path.dirname(filename)
 
+local SERVER_OPTIONS = {
+	key_file=path.."/tls_key", cert_file=path.."/tls_key.pub"}
+
+local CLIENT_OPTIONS = {ca_file=path.."/tls_key.pub"}
+
 
 return {
 	test_raw = function()
@@ -12,17 +17,17 @@ return {
 
 		local h = levee.Hub()
 
-		local function server()
-			local err, config = TLS.Config({
-				key_file=path.."/tls_key",
-				cert_file=path.."/tls_key.pub"})
+		local err, serve = h.stream:listen()
+		local err, addr = serve:addr()
 
-			local err, serve = h.stream:listen(9000)
+		local function server()
+			local err, config = TLS.Config(SERVER_OPTIONS)
 			local err, ctx = config:server()
 
 			local err, conn = serve:recv()
 			ctx:upgrade(conn)
 			conn:handshake()
+
 			assert.equal(conn:reads(), "foo")
 			conn:write("bar")
 			conn:close()
@@ -31,15 +36,18 @@ return {
 		h:spawn(server)
 
 		-- client
-		local err, config = TLS.Config({ca_file=path.."/tls_key.pub"})
+		local err, conn = h.stream:connect(addr:port())
 
-		local err, conn = h.stream:connect(9000)
-
+		local err, config = TLS.Config(CLIENT_OPTIONS)
 		local err, conn = config:upgrade(conn, "www.imgix.com")
 		conn:handshake()
+
 		conn:write("foo")
 		assert.equal(conn:reads(), "bar")
 		assert.equal(conn:reads(), nil)
+	end,
+
+	test_core = function()
 	end,
 
 	test_handshake_fail = function()
