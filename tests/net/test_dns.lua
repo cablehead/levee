732a9d8bc4ab798a05cdc74b1b1edaa66b631f58
Andres Buritica <andres@thelinuxkid.com>
2017-05-17 00:36:16 -0700

    mock dns server in net.dns unit tests

--- tests/net/test_dns.lua
+++ tests/net/test_dns.lua
@@ -3,13 +3,71 @@ local _ = levee._
 
 local errors = require("levee.errors")
 
+local filename = debug.getinfo(1, 'S').source:sub(2)
+local path = _.path.dirname(filename)
+
+
+-- Normally this is 768. Here we make room for the leading 16-bit QID
+local DNS_PACK_SIZE = 766
+
+
+local function response(rtype)
+	local records = {
+		["GOOGLE-A"]={size=766, file="dns-google-a.data"},
+		["GOOGLE-B"]={size=766, file="dns-google-b.data"},
+		["GOOGLE-A-AAAA"]={size=2298, file="dns-google-a-aaaa.data"},
+		["GOOGLE-ADDR"]={size=2298, file="dns-google-addr.data"},
+		["YAHOO-A"]={size=766, file="dns-yahoo-a.data"},
+	}
+
+	local f = io.open(path.."/"..records[rtype].file, "rb")
+	local data = f:read(records[rtype].size)
+	f:close()
+
+	return data
+end
+
+local function respond(server, rtype)
+	local buf = levee.d.Buffer(4096)
+	local err, who, n = server:recvfrom(buf:tail())
+	buf:bump(n)
+
+	local qid = string.sub(buf:take(), 1,2)
+	local data = response(rtype)
+	local size = data:len()
+
+	for i=0,(size/DNS_PACK_SIZE)-1 do
+		local off = 0
+		local packet = string.sub(data, i*DNS_PACK_SIZE+1, (i+1)*DNS_PACK_SIZE)
+		packet = qid..packet
+		local remain = packet:len()
+		while remain > 0 do
+				local chunk = string.sub(packet, off+1, off+DNS_PACK_SIZE)
+				local err, n = server:sendto(who, chunk, DNS_PACK_SIZE)
+				if n < 0 then n = 0 end
+				off = off + n
+				remain = remain - n
+		end
+	end
+end
+
 
 return {
 	test_core = function()
 		local h = levee.Hub()
-		local err, resv = h.dns:resolver()
-		assert(not err)
 
+		local addr = "127.0.0.1"
+		local port = 1053
+
+		local function server()
+			local err, s = h.dgram:bind(port, addr)
+			respond(s, "GOOGLE-A")
+			s:close()
+		end
+		h:spawn(server)
+
+		local err, resv = h.dns:resolver(port, addr)
+		assert(not err)
 		local err, records = resv:query("google-public-dns-a.google.com", "A")
 		assert(not err)
 		assert.equal(#records, 1)
@@ -36,8 +94,18 @@ return {
 
 	test_aaaa = function()
 		local h = levee.Hub()
-		local err, resv = h.dns:resolver()
 
+		local addr = "127.0.0.1"
+		local port = 1053
+
+		local function server()
+			local err, s = h.dgram:bind(port, addr)
+			respond(s, "GOOGLE-A-AAAA")
+			s:close()
+		end
+		h:spawn(server)
+
+		local err, resv = h.dns:resolver(port, addr)
 		local err, records = resv:query("google-public-dns-a.google.com", "AAAA")
 		local expect = {
 			name="google-public-dns-a.google.com.",
@@ -53,8 +121,18 @@ return {
 
 	test_multi_query = function()
 		local h = levee.Hub()
-		local err, resv = h.dns:resolver()
 
+		local addr = "127.0.0.1"
+		local port = 1053
+
+		local function server()
+			local err, s = h.dgram:bind(port, addr)
+			respond(s, "GOOGLE-A")
+			s:close()
+		end
+		h:spawn(server)
+
+		local err, resv = h.dns:resolver(port, addr)
 		local err, records = resv:query("google-public-dns-a.google.com", "A")
 		local expect = {
 				name="google-public-dns-a.google.com.",
@@ -65,6 +143,13 @@ return {
 		}
 		assert.same(records[1], expect)
 
+		local function server()
+			local err, s = h.dgram:bind(port, addr)
+			respond(s, "GOOGLE-B")
+			s:close()
+		end
+		h:spawn(server)
+
 		err, records = resv:query("google-public-dns-b.google.com", "A")
 		local expect = {
 				name="google-public-dns-b.google.com.",
@@ -74,6 +159,13 @@ return {
 				section="ANSWER"
 		}
 		assert.same(records[1], expect)
+
+		local function server()
+			local err, s = h.dgram:bind(port, addr)
+			respond(s, "GOOGLE-A-AAAA")
+			s:close()
+		end
+		h:spawn(server)
 
 		local err, records = resv:query("google-public-dns-a.google.com", "AAAA")
 		local expect = {
@@ -90,11 +182,19 @@ return {
 
 	test_multi_records = function()
 		local h = levee.Hub()
-		local err, resv = h.dns:resolver()
-		assert(not err)
 
+		local addr = "127.0.0.1"
+		local port = 1053
+
+		local function server()
+			local err, s = h.dgram:bind(port, addr)
+			respond(s, "YAHOO-A")
+			s:close()
+		end
+		h:spawn(server)
+
+		local err, resv = h.dns:resolver(port, addr)
 		local err, records = resv:query("yahoo.com", "A")
-		assert(not err)
 		assert.equal(#records, 3)
 		table.sort(records)
 		local expect = {
@@ -127,11 +227,19 @@ return {
 
 	test_address = function()
 		local h = levee.Hub()
-		local err, resv = h.dns:resolver()
-		assert(not err)
 
-		local err, records = resv:query("8.8.4.4.", "A")
-		assert(not err)
+		local addr = "127.0.0.1"
+		local port = 1053
+
+		local function server()
+			local err, s = h.dgram:bind(port, addr)
+			respond(s, "GOOGLE-ADDR")
+			s:close()
+		end
+		h:spawn(server)
+
+		local err, resv = h.dns:resolver(port, addr)
+		local err, records = resv:query("8.8.4.4", "A")
 		assert.equal(#records, 1)
 		local expect = {
 			name="8.8.4.4.",
