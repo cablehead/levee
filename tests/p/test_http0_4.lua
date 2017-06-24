5a809822a0b077578a4427aa33537a52b91d958d
Andres Buritica <andres@thelinuxkid.com>
2017-06-24 16:02:47 -0700

    add test for encoding post request

--- tests/p/test_http0_4.lua
+++ tests/p/test_http0_4.lua
@@ -15,11 +15,11 @@ return {
 		-- use an array to maintain params order
 		local params = {"fe", "fi 😬"}
 		local headers = {fa="fe", fi="fo"}
-		local data = "fum\n"
 		local buf = Buffer(4096)
 
-		local err = HTTP.encode_request("GET", path, params, headers, data, buf)
+		local err = HTTP.encode_request("GET", path, params, headers, nil, buf)
 		assert(not err)
+
 
 		local p = parser.Request()
 		p:init_request()
@@ -37,6 +37,60 @@ return {
 		len = len - rc
 
 		local headers = {}
+		for i=1,4 do
+			local err, rc = p:next(buf, len)
+			assert(rc > 0)
+			assert.equal(p:is_done(), false)
+			local k,v = p:value(buf)
+			headers[k] = v
+			buf = buf + rc
+			len = len - rc
+		end
+
+		local want = {
+			["User-Agent"]=USER_AGENT,
+			Accept="*/*",
+			fa="fe",
+			fi="fo",
+		}
+		assert.same(headers, want)
+
+		local err, rc = p:next(buf, len)
+		assert(rc > 0)
+		assert.equal(p:is_done(), true)
+	end,
+
+	test_encode_request_post = function()
+		local HTTP = require("levee.p.http.0_4")
+		local Buffer = require("levee.d.buffer")
+		local parser = require("levee.p.http").parser
+
+		local path = "/fa"
+		-- use an array to maintain params order
+		local params = {"fe", "fi 😬"}
+		local headers = {fa="fe", fi="fo"}
+		local data = "fum\n"
+		local buf = Buffer(4096)
+
+		local err = HTTP.encode_request("POST", path, params, headers, data, buf)
+		assert(not err)
+
+		local p = parser.Request()
+		p:init_request()
+
+		buf = buf:value()
+		local err, rc = p:next(buf, 5)
+		assert.equal(rc, 0)
+
+		local len = 130
+		local err, rc = p:next(buf, len)
+		assert(rc > 0)
+		assert.equal(p:is_done(), false)
+		assert.same({p:value(buf)}, {"POST", "/fa?1=fe&2=fi+%F0%9F%98%AC", 1})
+		buf = buf + rc
+		len = len - rc
+
+		local headers = {}
 		for i=1,5 do
 			local err, rc = p:next(buf, len)
 			assert(rc > 0)
@@ -49,7 +103,7 @@ return {
 
 		local want = {
 			["User-Agent"]=USER_AGENT,
-			["Content-Length"]= "4",
+			["Content-Length"]="4",
 			Accept="*/*",
 			fa="fe",
 			fi="fo",
